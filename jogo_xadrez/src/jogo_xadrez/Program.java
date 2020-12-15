781ca71d75fbb3110d3a41a595ff74b9f5c7615c
Jocel Viana <jocelviana@gmail.com>
2020-12-15 19:26:06 -0200

    Projeto modificado

--- jogo_xadrez/src/jogo_xadrez/Program.java
+++ jogo_xadrez/src/jogo_xadrez/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
 		System.out.println("Outro commit");
+		System.out.println("Mais um commit");
 
 	}
 
